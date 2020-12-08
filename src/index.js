36a800be3ef77caf1499f6d3b29cc2414bf585db
Anders L. Hurum <andershurum@gmail.com>
2020-12-08 18:08:21 +0100

    enh(app): added bootstrap, made app more fun

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => (
-    <div><h1>Hello, world!</h1></div>
-)
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+
+function App(props) {
+    const [count, setCount] = React.useState(0);
+
+    return <Container>
+        <Row className="justify-content-md-center">
+          <Col xs lg="2"></Col>
+          <Col md="auto">
+            <Row><Col><h3 className="text-center">How many fish can the dolphin eat?</h3></Col></Row>
+            <Row>
+              <Col>
+                <p className="text-center">{props.text} : {count}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Button block bsStyle="primary" bsSize='large' onClick={ () => setCount(count+1) }>🐟🐟🐟</Button>
+            </Row>
+          </Col>
+          <Col xs lg="2"></Col>
+        </Row>
+    </Container>
+}
+
 
 ReactDOM.render(
-    <App/>,
+    <App text="🐬" />,
     document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,13 @@ module.exports = {
                 ],
             },
             {
+                test: /\.css$/,
+                use: [
+                  "style-loader",
+                  "css-loader"
+                ]
+            },
+            {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 loader: "babel-loader",
